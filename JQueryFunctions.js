6a291e23155cff09dc4ca15172318ad70c4ff9e9
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2024-03-29 11:48:46 +0500

    Different functionalities are impelemented using JQuery like hovering, displaying tooltip, sorting list

--- JQueryFunctions.js
+++ JQueryFunctions.js
@@ -44,4 +44,49 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    // When the button is clicked, open the modal
+    $("#openModalBtn").click(function(){
+        $("#myModal").css("display", "block");
+    });
 
+    // When the close button or outside of the modal is clicked, close the modal
+    $(".close, .modal").click(function(event){
+        if (event.target === this) {
+            $("#myModal").css("display", "none");
+        }
+    });
+});
+
+$(document).ready(function() {
+    // Show tooltip on hover
+    $("#myTooltip").hover(function() {
+        $(this).find(".tooltiptext").css("visibility", "visible").css("opacity", 1 );
+    }, function() {
+        $(this).find(".tooltiptext").css("visibility", "hidden").css("opacity", 0);
+    });
+});
+
+$(document).ready(function() {
+    // Function to sort the list alphabetically
+    function sortList() {
+        // Get the list items
+        var list = $("#itemList");
+        var items = list.children("li").get();
+        
+        // Sort the items alphabetically
+        items.sort(function(a, b) {
+            var textA = $(a).text().toUpperCase();
+            var textB = $(b).text().toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+        
+        // Update the list with sorted items
+        $.each(items, function(index, item) {
+            list.append(item);
+        });
+    }
+
+    // Add click event listener to the sort button
+    $("#sortButton").click(sortList);
+});
