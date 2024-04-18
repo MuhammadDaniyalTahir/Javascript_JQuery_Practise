de4d1c2f45fbc5e9734e04a5f759efe40886d3d0
Muhammad Daniyal Tahir <muhammaddaniyaltahir969@gmail.com>
2024-04-18 20:25:44 +0500

    Data from api is successfully fetched

--- fetchDatafromApiJS.js
+++ fetchDatafromApiJS.js
@@ -1,22 +1,34 @@
-function fetchData(){
-    $(document).ready(function(event){
+
+$(document).ready(function (event) {
+
+    $("#searchForm").submit(function (event) {
         event.preventDefault();
 
         let searchData = $("#searchInput").val();
-        
+
         $.ajax({
-            url:"http://universities.hipolabs.com/search",
+            url: "http://universities.hipolabs.com/search",
             method: "GET",
-            data:{name, searchData},
-            success: function(response){
+            data: { name: searchData },
+            success: function (response) {
                 displayDatafunc(response);
             },
-            error: function(xhr, status, error){
-                
+            error: function (xhr, status, error) {
+
             }
         });
+    });
 
+});
+function displayDatafunc(response) {
+
+    $.each(response, function (index, university) {
+        let heading = $("<h2>").text(university.name);
+
+        let country = $("<p>").text(university.country);
+
+        let link = $("<a>").attr("href", university.web_pages[0]).text("view page");
+
+        $("#DisplayData").append(heading, country, link);
     });
 }
-
-
